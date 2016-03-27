ac1102bd507ed4157ba37473338aadd84d9d0479
nimzo6689 <nimzo.i.d.6689@gmail.com>
2016-03-27 15:56:10 +0900

    Initial commit for project

---
+++ app/src/main/java/com/hatenablog/nimzo/calculator/Calculator.java
@@ -0,0 +1,105 @@
+package com.hatenablog.nimzo.calculator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Calculator extends AppCompatActivity {
+
+    TextView textView;
+    EditText editText;
+    Button button;
+
+    int recentOperator = R.id.button_equal;
+    double result;
+    boolean isOperatorKeyPushed;
+
+    View.OnClickListener buttonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            textView.setText(editText.getText().toString());
+        }
+    };
+
+    View.OnClickListener buttonNumberListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Button button = (Button) view;
+
+            if (isOperatorKeyPushed == true) {
+                editText.setText(button.getText());
+            } else {
+                editText.append(button.getText());
+            }
+            isOperatorKeyPushed = false;
+        }
+    };
+
+    View.OnClickListener buttonOperatorListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Button operatorButton = (Button) view;
+            double value = Double.parseDouble(editText.getText().toString());
+            if (recentOperator == R.id.button_equal) {
+                result = value;
+            } else {
+                result = calc(recentOperator, result, value);
+                editText.setText(String.valueOf(result));
+            }
+
+            recentOperator = operatorButton.getId();
+            textView.setText(operatorButton.getText());
+            isOperatorKeyPushed = true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_calclator);
+
+        textView = (TextView) this.findViewById(R.id.textview);
+        editText = (EditText) this.findViewById(R.id.edittext);
+        button = (Button) this.findViewById(R.id.button);
+        button.setOnClickListener(buttonListener);
+
+        findViewById(R.id.button_1).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.button_2).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.button_3).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.button_4).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.button_5).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.button_6).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.button_7).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.button_8).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.button_9).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.button_0).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.button_dot).setOnClickListener(buttonNumberListener);
+
+        findViewById(R.id.button_add).setOnClickListener(this.buttonOperatorListener);
+        findViewById(R.id.button_subtract).setOnClickListener(this.buttonOperatorListener);
+        findViewById(R.id.button_multiply).setOnClickListener(this.buttonOperatorListener);
+        findViewById(R.id.button_divide).setOnClickListener(this.buttonOperatorListener);
+        findViewById(R.id.button_equal).setOnClickListener(this.buttonOperatorListener);
+    }
+
+    private double calc(int operator, double value1, double value2) {
+        switch (operator) {
+            case R.id.button_add:
+                return value1 + value2;
+            case R.id.button_subtract:
+                return value1 - value2;
+            case R.id.button_multiply:
+                return value1 * value2;
+            case R.id.button_divide:
+                if (value2 == 0) {
+                    return 0;
+                }
+                return value1 / value2;
+            default:
+                return value1;
+        }
+    }
+}
